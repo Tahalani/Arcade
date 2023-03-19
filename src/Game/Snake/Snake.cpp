6ab3ac4a345fb3cf919aa13dae8a65d0c7896879
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-03-19 22:13:06 +0100

    feat: algo for the snake

--- src/Game/Snake/Snake.cpp
+++ src/Game/Snake/Snake.cpp
@@ -7,24 +7,157 @@
 
 #include "Snake.hpp"
 
-// Snake::Snake()
-// {
-// }
+Snake::Snake()
+{
+    _status = true;
+    _time = 0;
+    _is_apple = false;
+    _coord.insert({0, std::pair<std::size_t, std::size_t>(1, 4)});
+    _coord.insert({1, std::pair<std::size_t, std::size_t>(1, 3)});
+    _coord.insert({2, std::pair<std::size_t, std::size_t>(1, 2)});
+    _coord.insert({3, std::pair<std::size_t, std::size_t>(1, 1)});
+}
 
-// Snake::~Snake()
-// {
-// }
+Snake::~Snake()
+{
+}
 
-// void Snake::init()
-// {
-// }
+void Snake::runGame(std::size_t key)
+{
+    setTime();
+    double time = getTime();
+    if (time > 0.2) {
+        in_loop(key);
+        if (_is_loose == true)
+            restart();
+        setClock();
+    }
+}
 
-// void Snake::stop()
-// {
-// }
+void Snake::in_loop(std::size_t key)
+{
+    if (key == LEFT || key == RIGHT || key == UP || key == DOWN) {
+        handleSnake(key, map);
+    }
+}
 
-// extern "C" {
-//     IGame *entryGame() {
-//         return new Snake();
-//     }
-// }
+void Snake::restart()
+{
+    _coord.clear();
+    _coord.insert({0, std::pair<std::size_t, std::size_t>(1, 4)});
+    _coord.insert({1, std::pair<std::size_t, std::size_t>(1, 3)});
+    _coord.insert({2, std::pair<std::size_t, std::size_t>(1, 2)});
+    _coord.insert({3, std::pair<std::size_t, std::size_t>(1, 1)});
+    _is_loose = false;
+    _status = true;
+    _time = 0;
+    map = {
+            "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "XOOOO -                    X",
+            "X                          X",
+            "X    -               -     X",
+            "X                          X",
+            "X         -                X",
+            "X                          X",
+            "X                          X",
+            "X                          X",
+            "X                          X",
+            "X                          X",
+            "X           -              X",
+            "X                          X",
+            "X                          X",
+            "X                     -    X",
+            "X                          X",
+            "X                          X",
+            "X    -                     X",
+            "X                          X",
+            "X                          X",
+            "X             -            X",
+            "X                      -   X",
+            "X                          X",
+            "X                          X",
+            "X                          X",
+            "X        -                 X",
+            "X                    -     X",
+            "X                          X",
+            "X                          X",
+            "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        };
+}
+
+void Snake::loose_condition(std::vector<std::string> map, std::size_t y, std::size_t x)
+{
+    if (map[y][x] == WALL || map[y][x] == SNAKE)
+        _is_loose = true;
+}
+
+void Snake::eat_apple(std::vector<std::string> map, std::size_t y, std::size_t x)
+{
+    if (map[y][x] == COIN)
+        _is_apple = true;
+}
+
+std::vector<std::string> Snake::addSnake_apple(std::vector<std::string>, std::size_t y, std::size_t x)
+{
+    if (_is_apple == true) {
+        _coord.insert({_coord.size(), {y, x}});
+        map[y][x] = SNAKE;
+        _is_apple = false;
+    }
+    return map;
+}
+
+std::vector<std::string> Snake::moveSnake(std::size_t y, std::size_t x)
+{
+    std::pair<std::size_t, std::size_t> oldCase;
+    std::pair<std::size_t, std::size_t> newCase;
+
+    oldCase.first = _coord.begin()->second.first;
+    oldCase.second = _coord.begin()->second.second;
+
+    for (auto it = _coord.begin(); it != _coord.end(); ++it) {
+        if (it == _coord.begin()) {
+            loose_condition(map, it->second.first + y, it->second.second + x);
+            eat_apple(map, it->second.first + y, it->second.second + x);
+            map[it->second.first + y][it->second.second + x] = SNAKE;
+            map[it->second.first][it->second.second] = EMPTY;
+            _coord[it->first] = {it->second.first + y, it->second.second + x};
+        } else {
+            map[oldCase.first][oldCase.second] = SNAKE;
+            map[it->second.first][it->second.second] = EMPTY;
+            newCase = {it->second.first, it->second.second};
+            _coord[it->first] = {oldCase.first, oldCase.second};
+            oldCase = {newCase.first, newCase.second};
+        }
+    }
+    map = addSnake_apple(map, oldCase.first, oldCase.second);
+    return (map);
+}
+
+std::size_t Snake::handleSnake(std::size_t key, std::vector<std::string> &map)
+{
+    const std::map<std::size_t, std::function<std::vector<std::string>
+    (std::size_t, std::size_t)>> ptr_fct = {
+        {LEFT, std::bind(&Snake::moveSnake, this, 0, -1)},
+        {RIGHT, std::bind(&Snake::moveSnake, this, 0, 1)},
+        {UP, std::bind(&Snake::moveSnake, this, -1, 0)},
+        {DOWN, std::bind(&Snake::moveSnake, this, 1, 0)}
+    };
+
+    map = ptr_fct.at(key)(0, 0);
+    return (0);
+}
+
+void Snake::setTime()
+{
+    for (size_t i = 0; i < 3141592; ++i)
+        _sink += sin(i);
+    clock_t end = clock();
+    _time = ((double) (end - _start)) / CLOCKS_PER_SEC;
+}
+
+extern "C" {
+    IGame *entryGame() {
+        return new Snake();
+    }
+}
