5b6c43a60907014da278f6b2c5b5785deed2ff01
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-03-29 10:21:19 +0200

    fix: good coord for the map of snake

--- src/Game/Snake/Snake.cpp
+++ src/Game/Snake/Snake.cpp
@@ -14,10 +14,10 @@ Snake::Snake()
     _score = 0;
     _is_apple = false;
     _key = 0;
-    _coord.insert({0, std::pair<std::size_t, std::size_t>(1, 4)});
-    _coord.insert({1, std::pair<std::size_t, std::size_t>(1, 3)});
-    _coord.insert({2, std::pair<std::size_t, std::size_t>(1, 2)});
-    _coord.insert({3, std::pair<std::size_t, std::size_t>(1, 1)});
+    _coord.insert({0, std::pair<std::size_t, std::size_t>(14, 15)});
+    _coord.insert({1, std::pair<std::size_t, std::size_t>(14, 14)});
+    _coord.insert({2, std::pair<std::size_t, std::size_t>(14, 13)});
+    _coord.insert({3, std::pair<std::size_t, std::size_t>(14, 12)});
     setClock();
 }
 
